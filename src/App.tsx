cb794a62a320c013f336935f9f554f43813c5824
Andrey <systerr@gmail.com>
2021-05-07 14:03:17 +0800

    more styles

--- src/App.tsx
+++ src/App.tsx
@@ -1,27 +1,42 @@
 import React from 'react';
+import Grid from '@material-ui/core/Grid';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import Typography from '@material-ui/core/Typography';
+
 import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
 
-import logo from './logo.svg';
-import './App.css';
+import cls from './app.module.scss';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div className={cls.app}>
+        <CssBaseline />
+        <Grid container spacing={0}>
+          <Grid item xs={2}>
+            <Link to="/">Home</Link>
+            <Link to="/about">About</Link>
+            <Link to="/users">Users</Link>
+          </Grid>
+          <Grid item xs={10}>
+            <AppBar position="sticky">
+              <Toolbar>
+                <Typography component="h1" variant="h6" color="inherit" noWrap>
+                  Dashboard
+                </Typography>
+              </Toolbar>
+            </AppBar>
+            <Switch>
+              <Route path="/about">About</Route>
+              <Route path="/users">Users</Route>
+              <Route path="/">Home</Route>
+            </Switch>
+          </Grid>
+        </Grid>
+      </div>
+    </Router>
   );
 }
 
