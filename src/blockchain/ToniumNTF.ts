1703c204a30567f9bafe82e45a017ac74e75ba81
IhnatMukhin <ihnatmukhin@gmail.com>
2021-05-18 17:19:01 +0300

    example methods

--- src/blockchain/ToniumNTF.ts
+++ src/blockchain/ToniumNTF.ts
@@ -12,6 +12,50 @@ class TiniumNFT {
       this.setProvider(localStorage.getItem('toniumProvider') as string);
     }
   }
+  getAllTokkens() {
+    const tokens: [] = [];
+    return tokens;
+  } // получение всех токкенов аккаунта, которые купил и созданных(с разделением по типам)
+
+  getAllTransactions() {
+    const transactions: [] = [];
+    return transactions;
+  } // получение всех транзакций аккаунта (с разделением по типам)
+
+  postCreateCollections() {
+    const cpllections: [] = [];
+    return cpllections; // создание коллекции
+  }
+
+  postCreateTokken() {
+    const createdToken: {} = {};
+    return createdToken; // создание токкена
+  }
+
+  putEditCollection(tooken: {}) {
+    const editToken = {};
+    return editToken;
+  } //  редактирование колллекции
+
+  getAllNFTAuctions() {
+    const aunctions: [] = [];
+    return aunctions; // получение всех аукционов
+  }
+
+  postAunctionMark(auction: {}) {
+    const markAuction = {};
+    return markAuction; // mark auction
+  }
+
+  postAunctionBid(auction: {}) {
+    const bid = {};
+    return auction;
+  } // do bid
+
+  getAllNFTs() {
+    const nfts: [] = [];
+    return nfts; // получение всех нфтешек для радара
+  }
 
   getProviders() {
     return this.providers;
