aa3458bf713d3ad0937db0753ebb6daff8c321ff
IhnatMukhin <ihnatmukhin@gmail.com>
2021-05-22 20:31:11 +0300

    login

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 /* eslint-disable no-unused-vars */
-import React, { useState, useMemo, useReducer } from 'react';
+import React, { useState, useMemo, useReducer, useEffect } from 'react';
 import Grid from '@material-ui/core/Grid';
 
 import CssBaseline from '@material-ui/core/CssBaseline';
@@ -16,16 +16,13 @@ import TrackChangesIcon from '@material-ui/icons/TrackChanges';
 import TwitterIcon from '@material-ui/icons/Twitter';
 import TelegramIcon from '@material-ui/icons/Telegram';
 import PanToolIcon from '@material-ui/icons/PanTool';
-import CloseIcon from '@material-ui/icons/Close';
+
 /* eslint-disable react/jsx-props-no-spreading */
 import EditIcon from '@material-ui/icons/Edit';
 import Button from '@material-ui/core/Button';
-import Dialog from '@material-ui/core/Dialog';
-import TextField from '@material-ui/core/TextField';
-import IconButton from '@material-ui/core/IconButton';
-import MuiDialogTitle from '@material-ui/core/DialogTitle';
 
-import DialogContent from '@material-ui/core/DialogContent';
+import TextField from '@material-ui/core/TextField';
+
 import LinkIcon from '@material-ui/icons/Link';
 
 import FormControl from '@material-ui/core/FormControl';
@@ -36,15 +33,21 @@ import ToniumNFT from './blockchain/ToniumNTF';
 
 import Home from './pages/Home/Home';
 import Mint from './pages/Mint/Mint';
+import Login from './Components/Login';
 import Radar from './pages/Radar/Radar';
 import Auction from './pages/Auction/Auction';
 import Own from './pages/Own/Own';
 import Wp from './pages/Wp/Wp';
-import { ContextApp, reducer, intialState, setLogin } from './store/reducer';
+import {
+  ContextApp,
+  reducer,
+  intialState,
+  setOpen,
+  setLogin,
+} from './store/reducer';
 import cls from './app.module.scss';
 
 import Logo from './img/tonium-logo-dark-text.svg';
-import CristalIcon from './img/cristall.svg';
 
 // Application initialization
 
@@ -52,48 +55,22 @@ declare const window: any;
 
 function App() {
   const [state, dispatch] = useReducer(reducer, intialState);
-  const [open, setOpen] = useState(false);
-  const [
-    isAdditionalProviderFieldsRequired,
-    setIsAdditionalProviderFieldsRequired,
-  ] = useState(false);
-  const [selectedProvider, setSelectedProvider] = useState(null);
-  const [formValues, setFormValues] = useState<{} | null>(null);
 
+  // useEffect(() => {
+  //   (async () => {
+  //     if (initRequiredField) {
+  //       await initRequiredField
+  //         .validator(formValues?.[initRequiredField?.name])
+  //         .then((data: any): any => {
+  //           setValidate(data);
+  //         })
+  //         .catch((e: any) => console.error(e, 'Err'));
+  //     }
+  //   })();
+  // }, [initRequiredField]);
   const toniumNFT = useMemo(() => new ToniumNFT(state, dispatch), []);
   window.toniumNFT = toniumNFT;
-  const handleClose = () => {
-    setIsAdditionalProviderFieldsRequired(false);
-    setOpen(false);
-  };
 
-  const DialogTitle = (props: any) => {
-    const { children, onClose, ...other } = props;
-    return (
-      <MuiDialogTitle disableTypography className={cls.root} {...other}>
-        <Typography variant="h6">{children}</Typography>
-        {onClose ? (
-          <IconButton
-            aria-label="close"
-            className={cls.closeButton}
-            onClick={onClose}
-          >
-            <CloseIcon />
-          </IconButton>
-        ) : null}
-      </MuiDialogTitle>
-    );
-  };
-
-  const selectProvider = (name: string) => {
-    if (toniumNFT.getProviders()[name].getRequiredInitFields().length) {
-      setSelectedProvider(name as any);
-      setIsAdditionalProviderFieldsRequired(true);
-    } else {
-      toniumNFT.setProvider(name);
-      handleClose();
-    }
-  };
   return (
     <ContextApp.Provider value={{ dispatch, state }}>
       <Router>
@@ -230,25 +207,46 @@ function App() {
                         className={cls.address}
                       >
                         <Link to="/wp" className={cls.nick}>
-                          <span>@mrboss</span>
-                          <EditIcon />
+                          {/* <span>@mrboss</span> */}
+                          {/* <EditIcon /> */}
                         </Link>
-                        <form noValidate autoComplete="off">
-                          <TextField label="address" variant="filled" />
+                        <form
+                          noValidate
+                          autoComplete="off"
+                          style={{ display: 'flex' }}
+                        >
+                          <TextField
+                            label="address"
+                            variant="filled"
+                            InputProps={{
+                              readOnly: true,
+                            }}
+                          />
                         </form>
-                        <span className={cls.balance}>
+                        <Button
+                          variant="contained"
+                          color="primary"
+                          onClick={() => {
+                            toniumNFT.providerLogout();
+                            setLogin(dispatch, false);
+                          }}
+                          className={cls.logout}
+                        >
+                          Log Out
+                        </Button>
+                        {/* <span className={cls.balance}>
                           <span>12.23</span>
                           <img src={CristalIcon} alt="TON" width="20" />{' '}
-                        </span>
-                        <Link to="/wp" className={cls.linkwallet}>
+                        </span> */}
+                        {/* <Link to="/wp" className={cls.linkwallet}>
                           <LinkIcon style={{ fontSize: 30 }} />
-                        </Link>
+                        </Link> */}
                       </Grid>
                     ) : (
                       <Button
                         variant="contained"
                         color="primary"
-                        onClick={() => setOpen(true)}
+                        onClick={() => setOpen(dispatch, true)}
                       >
                         Login
                       </Button>
@@ -300,133 +298,7 @@ function App() {
           justify="space-around"
           alignItems="center"
         >
-          <Dialog
-            onClose={() => handleClose}
-            aria-labelledby="customized-dialog-title"
-            open={open}
-          >
-            <Grid item xs={12} className={cls.poapup}>
-              <DialogTitle
-                id="customized-dialog-title"
-                onClose={handleClose}
-                className={cls.poapup__title}
-              />
-              <DialogContent className={cls.poapup__content}>
-                Plase select provider (TODO markup)
-                {Object.entries(toniumNFT.getProviders()).map(
-                  ([providerName, provider]) => (
-                    <div key={providerName}>
-                      <Button
-                        variant="outlined"
-                        color="primary"
-                        disabled={!provider.isAvailable()}
-                        onClick={() => selectProvider(providerName)}
-                      >
-                        {providerName}
-                      </Button>
-                    </div>
-                  ),
-                )}
-              </DialogContent>
-
-              {isAdditionalProviderFieldsRequired && (
-                <form
-                  autoComplete="off"
-                  className={cls.mnemonic}
-                  onSubmit={(data) => console.log(data)}
-                >
-                  <Grid
-                    container
-                    xs={12}
-                    direction="row"
-                    justify="center"
-                    style={{ margin: '50px 0' }}
-                  >
-                    <Grid item xs={7}>
-                      {toniumNFT
-                        .getProviders()
-                        [selectedProvider as any].getRequiredInitFields()
-                        .map((field: any) => {
-                          const check = field
-                            .validator(formValues?.[`${field.name}`])
-                            .then((data: {}): any => console.log(data, 'DATA'))
-                            .catch((e: any) => console.log(e, 'Err'));
-                          return (
-                            <TextField
-                              key={field.name}
-                              onChange={(e) => {
-                                setFormValues((fv: {}) => {
-                                  let fvLocal = fv;
-                                  if (!fvLocal) {
-                                    fvLocal = {};
-                                  }
-                                  fvLocal[field.name] = e.target.value;
-                                  return fvLocal;
-                                });
-                              }}
-                              id={field.name}
-                              label={field.description}
-                              fullWidth
-                              value={formValues?.[`${field.name}`] || ''}
-                              helperText={
-                                !formValues?.[`${field.name}`]
-                                  ? 'This field is required'
-                                  : ''
-                              }
-                            />
-                          );
-                        })}
-                    </Grid>
-                    <Grid item xs={3}>
-                      {toniumNFT
-                        .getProviders()
-                        [selectedProvider as any].getInitActions()
-                        .map((action: any) => (
-                          <Button
-                            variant="outlined"
-                            key={action.name}
-                            color="primary"
-                            id={action.name}
-                            style={{ height: '100%', marginLeft: 20 }}
-                            onClick={() => {
-                              const nameValue = { ...formValues };
-                              action
-                                .action()
-                                .then((data: any) => {
-                                  nameValue[`${action.name}`] = data;
-                                  setFormValues(nameValue);
-                                  console.log(formValues);
-                                })
-                                .catch((e: any) => console.error(e.message));
-                            }}
-                          >
-                            {action.description}
-                          </Button>
-                        ))}
-                    </Grid>
-                  </Grid>
-                  <Grid container xs={12} direction="row" justify="center">
-                    <Button
-                      disabled={!formValues}
-                      variant="contained"
-                      color="primary"
-                      type="submit"
-                      onClick={() => {
-                        // eslint-disable-next-line no-debugger
-                        toniumNFT.setProvider(
-                          selectedProvider as any,
-                          formValues as {},
-                        );
-                        handleClose();
-                      }}
-                    >
-                      Login
-                    </Button>
-                  </Grid>
-                </form>
-              )}
-            </Grid>
-          </Dialog>
+          <Login toniumNFT={toniumNFT} />
         </Grid>
       </Router>
     </ContextApp.Provider>
