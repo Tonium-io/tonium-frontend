8735a654109ddc787f7ae3e70606ef18f078a238
IhnatMukhin <ihnatmukhin@gmail.com>
2021-05-28 16:22:32 +0300

    fix

--- src/pages/Collection/Collection.tsx
+++ src/pages/Collection/Collection.tsx
@@ -26,8 +26,16 @@ const Collection = () => {
       setLoad(true);
       // to do ajax
       const payload = [
-        { name: 'Test1', address: '0:x12312312432534453' },
-        { name: 'Test2', address: '0:x12312332425532535' },
+        {
+          name: 'Test1',
+          address: '0:x12312312432534453',
+          img: 'https://pobedarf.ru/wp-content/uploads/2020/11/depositphotos_98492334_l-2015-pic4_zoom-1500x1500-71566.jpg',
+        },
+        {
+          name: 'Test2',
+          address: '0:x12312332425532535',
+          img: 'https://pobedarf.ru/wp-content/uploads/2020/11/depositphotos_98492334_l-2015-pic4_zoom-1500x1500-71566.jpg',
+        },
       ];
       setUserCollenctionTokens(dispatch, payload);
       setLoad(false);
