d0fbe97cfd5aa03e6824d694aabd3af2ccc308e7
raAndrievsyi <raAndrievskyi@gmail.com>
2021-06-12 12:53:25 +0300

    transaction fix 2.0

--- src/App.tsx
+++ src/App.tsx
@@ -41,6 +41,9 @@ function App() {
             <Route exact path="/own">
               <Own />
             </Route>
+            <Route exact path="/own/transaction">
+              <Transaction />
+            </Route>
             <Route exact path="/collections">
               <Mint />
             </Route>
